af47a667bc787e318324359b1e8907375bac3a53
vnoe36781 <123997119+vnoe36781@users.noreply.github.com>
2025-05-28 14:03:55 -0400

    Add files via upload

---
+++ team_locations.py
@@ -0,0 +1,54 @@
+
+# team_locations.py
+
+NFL_COORDS = {
+    "BUF": (42.7738, -78.7869),
+    "KC": (39.049, -94.4839),
+    # ... all other NFL teams
+}
+
+NBA_COORDS = {
+    "LAL": (34.043, -118.2673),
+    "BOS": (42.3663, -71.0622),
+    # ... all other NBA teams
+}
+
+MLB_COORDS = {
+    "NYY": (40.8296, -73.9262),
+    "LAD": (34.0739, -118.2400),
+    # ... all other MLB teams
+}
+
+FBS_COORDS = {
+    "Alabama": (33.2081, -87.5504),
+    "Ohio State": (40.0017, -83.0197),
+    # ... all FBS programs
+}
+
+FCS_COORDS = {
+    "Montana State": (45.6674, -111.0460),
+    "Jackson State": (32.2965, -90.1790),
+    # ... all FCS programs
+}
+
+DII_COORDS = {
+    "Valdosta State": (30.8456, -83.2856),
+    "Ferris State": (43.6873, -85.4836),
+    # ... all DII football schools
+}
+
+NCAA_BASKETBALL_COORDS = {
+    "Duke": (36.0014, -78.9382),
+    "UConn": (41.8064, -72.2545),
+    # ... top 1000 M/W programs by ranking
+}
+
+TEAM_COORDS = {
+    "NFL": NFL_COORDS,
+    "NBA": NBA_COORDS,
+    "MLB": MLB_COORDS,
+    "FBS": FBS_COORDS,
+    "FCS": FCS_COORDS,
+    "DII": DII_COORDS,
+    "NCAAB": NCAA_BASKETBALL_COORDS,
+}
