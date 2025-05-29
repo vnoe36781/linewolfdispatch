426a54a19a5e89e67c9ece877df32652ff85f54c
vnoe36781 <123997119+vnoe36781@users.noreply.github.com>
2025-05-28 23:16:38 -0400

    Create d2_coords.py

---
+++ d2_coords.py
@@ -0,0 +1,9 @@
+FCS_COORDS = {
+  'Alabama A&M': (34.7829, -86.5681), 
+  'Alabama State': (32.3625, -86.294), 
+  'Albany': (42.6864, -73.8239), 
+  'Alcorn State': (32.0004, -91.1423), 
+  'Arkansas-Pine Bluff': (34.229, -92.0032), 
+  'Austin Peay': (36.5323, -87.3548), 
+  'Bethune-Cookman': (29.1874, -81.046), 
+}
